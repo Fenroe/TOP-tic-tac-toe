6d49c18c07113be5842d62f9c8e6cc37fd00b28a
Fenroe <scrambles9494@gmail.com>
2021-07-21 03:30:01 +0300

    :(

--- script/index.js
+++ script/index.js
@@ -1,6 +1,38 @@
+const player = token => {
+    capturedTiles = [];
+    status = notWon;
+    return {token, capturedTiles, status};
+};
+
 const gameBoard = (() => {
+    const winConditions = [
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9],
+        [1, 4, 7],
+        [2, 5, 8],
+        [3, 6, 9],
+        [1, 5, 9],
+        [3, 5, 7]
+    ]
+
+    let turnCount = 1;
+    let currentPlayer = "";
+    let winner = "";
+    return {
+        winConditions,
+        turnCount,
+        currentPlayer,
+        winner
+    }
+})(); 
+
+const displayController = (() => {
     const board = document.querySelector(".gameboard");
     const boardSpace = 9;
+
+    let playerOne = {};
+    let playerTwo = {};
 
     const boardHeading = () => {
         let headingContainer = document.createElement("div");
@@ -25,26 +57,54 @@ const gameBoard = (() => {
         let tile = document.createElement("div");
         tile.classList.add("gameboard-tile");
         tile.setAttribute("data-index-number", index);
+        tile.innerHTML = index;
         return tile;
     }
 
     const pregame = () => {
         board.innerHTML = "";
+        board.id = "pregame-state";
         board.append(boardHeading(), boardButtons("X"), boardButtons("O"));
+        buttons = board.querySelectorAll("button");
+        buttons.forEach(button => {
+            button.addEventListener("click", () => {
+                startGame(button.dataset.token);
+            })
+        })
     }
 
-    const startGame = () => {
-        board.innerHTML = "";
-        board.id = "pregame-state";
-        for(i=1; i<=boardSpace; i++) {
-            board.append(boardTile(i));
+    const startGame = (token) => {
+        playerOne = player(token);
+        if(token === "X") {
+            playerTwo = player("O");
+        } else {
+            playerTwo = player("X");
         }
+        board.innerHTML = "";
+        board.id = "game-state";
+        for(i=1; i<=boardSpace; i++) {
+            tile = boardTile(i);
+            board.append(tile);
+        }      
+    }
+
+    const gameTurn = token => {
+        tiles = board.querySelectorAll(".gameboard-tile");
+        tiles.forEach(tile => {
+            tile.addEventListener("click", () => {
+                tile.innerHTML = token;
+            })
+        })
     }
 
     return {
-        pregame, startGame
+        pregame, 
+        startGame, 
+        gameTurn,
+        playerOne,
+        playerTwo
     };
 
-})(); 
+})();
 
-gameBoard.pregame();
+displayController.pregame();
