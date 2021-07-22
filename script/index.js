8b1dad08612f20003694856be0bab46a6e760484
Fenroe <scrambles9494@gmail.com>
2021-07-22 04:08:11 +0300

    finished for now

--- script/index.js
+++ script/index.js
@@ -9,42 +9,45 @@ const player = token => {
 
 const gameBoard = (() => {
     const winConditions = [
-        [1, 2, 3],
-        [4, 5, 6],
-        [7, 8, 9],
-        [1, 4, 7],
-        [2, 5, 8],
-        [3, 6, 9],
-        [1, 5, 9],
-        [3, 5, 7]
+        ["1", "2", "3"],
+        ["4", "5", "6"],
+        ["7", "8", "9"],
+        ["1", "4", "7"],
+        ["2", "5", "8"],
+        ["3", "6", "9"],
+        ["1", "5", "9"],
+        ["3", "5", "7"]
     ]
 
-    let turnCount = 1;
-    let currentPlayer = "";
-    let winner = "";
+    let turnCount = 0;
     let currentTurn = "X";
     let nextTurn = "O";
 
-    const turnChange = () => {
-        let turnPast = gameBoard.currentTurn;
-        gameBoard.currentTurn = gameBoard.nextTurn;
-        gameBoard.nextTurn = turnPast;
+    const checkForWinner = playerX => {
+        gameBoard.winConditions.forEach(winCondition => {
+            if(playerX.capturedTiles.includes(winCondition[0]) === true &&
+            playerX.capturedTiles.includes(winCondition[1]) === true && 
+            playerX.capturedTiles.includes(winCondition[2]) === true) {
+                playerX.winner = true;
+                return;
+            }
+        })
+
     }
 
     return {
         winConditions,
         turnCount,
-        currentPlayer,
-        winner,
         currentTurn,
         nextTurn,
-        turnChange    
+        checkForWinner
     }
 
 })(); 
 
 const displayController = (() => {
     const board = document.querySelector(".gameboard");
+    const boardInfo = document.querySelector(".game-info-text");
     const boardSpace = 9;
 
     let gameTiles = [];
@@ -75,14 +78,20 @@ const displayController = (() => {
         return tile;
     }
 
+    const updateBoardInfo = text => {
+        boardInfo.innerHTML = text;
+    }
+
     const pregame = () => {
         board.innerHTML = "";
+        updateBoardInfo("");
         board.id = "pregame-state";
         board.append(boardHeading(), boardButtons("X"), boardButtons("O"));
     }
 
     const startGame = () => {
         board.id = "game-state";
+        gameBoard.turncount = 0;
         fillGameTiles();
     }
 
@@ -90,6 +99,7 @@ const displayController = (() => {
         board.innerHTML = "";
         gameTiles = [];
         for(i=1; i<=boardSpace; i++) {
+            i = String(i);
             if(playerOne.capturedTiles.includes(i)) {
                 tile = boardTile(playerOne.token);
                 tile.innerHTML = playerOne.token;
@@ -101,13 +111,52 @@ const displayController = (() => {
             }
             gameTiles.push(tile);
             board.append(tile);
-            console.log(tile.dataset.index);
         }      
+        switch(gameBoard.turnCount) {
+            case 9:
+                gameBoard.checkForWinner(playerOne);
+                gameBoard.checkForWinner(playerTwo)
+                postGame();
+                return;
+            case 5: case 6: case 7: case 8:
+                gameBoard.checkForWinner(playerOne);
+                gameBoard.checkForWinner(playerTwo);
+                if (playerOne.winner === true || playerTwo.winner === true) {
+                    postGame();
+                    return;
+                }
+                break;
+            default:
+                break;                
+        }
+        updateBoardInfo(gameBoard.currentTurn+"'s Move");
         gameStartListeners(gameTiles);
     };
 
     const tileCapture = (tile, token) => {
         tile.innerHTML = token;
+    }
+
+    const postGame = () => {
+        if(playerOne.winner === true) {
+            updateBoardInfo(playerOne.token+" Wins <br>Click the board to play again.");
+        }
+        else if(playerTwo.winner === true) {
+            updateBoardInfo(playerTwo.token+" Wins <br>Click the board to play again.");
+        }
+        else {
+            updateBoardInfo("It's A Tie <br>Click the board to play again.");
+        }
+        postGameListener();
+    }
+
+    const postGameListener = () => {
+        board.addEventListener("click", () => {
+            console.log("test");
+            playerOne = {};
+            playerTwo = {};
+            startGame();
+        })
     }
 
     return {
@@ -127,21 +176,25 @@ preGameListeners = () => {
         button.addEventListener("click", () => {
             playerOne = player(button.dataset.token);
             button.dataset.token === "X" ? playerTwo = player("O") : playerTwo = player("X");
-            displayController.startGame();
+            displayController.startGame(button.dataset.token);
         })
     })
 }
 
 gameStartListeners = (tiles) => {
     tiles.forEach(tile => {
-        if(tile.index !== "X" && tile.index !== "O") {
+        if(tile.dataset.index !== "X" && tile.dataset.index !== "O") {
             tile.addEventListener("click", () => {
-                if(playerOne.token === displayController.currentTurn) {
+                if(playerOne.token === gameBoard.currentTurn) {
                     playerOne.capturedTiles.push(tile.dataset.index);
+                    gameBoard.nextTurn = playerOne.token;
+                    gameBoard.currentTurn = playerTwo.token;
                 } else {
                     playerTwo.capturedTiles.push(tile.dataset.index);
-                };                
-                gameBoard.turnChange();
+                    gameBoard.nextTurn = playerTwo.token;
+                    gameBoard.currentTurn = playerOne.token;
+                };  
+                gameBoard.turnCount++;             
                 displayController.fillGameTiles();
             })
         } else return;
